c3732d9b7d6f8dafd32bf8b33af207b564a1e736
Mirroy2003 <luis.eduardo.200325@gmail.com>
2024-09-27 07:45:31 -0500

    feat(containers) : add containers models

---
+++ src/app/containers/model/container-model/container.entity.ts
@@ -0,0 +1,18 @@
+export class Container {
+  id: string;
+  status: string;
+  temperature: string;
+  humidity:string;
+  lastSync:string;
+  idTemplates: string; // Agregar esta propiedad
+
+  constructor(id: string, name: string, capacity: string,humidity:string,lastSync:string,idTemplates:string) {
+    this.id = id;
+    this.status = name;
+    this.temperature = capacity;
+    this.humidity = humidity;
+    this.lastSync = lastSync;
+    this.idTemplates = idTemplates;
+
+  }
+}
