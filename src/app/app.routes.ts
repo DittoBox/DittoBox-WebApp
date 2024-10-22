6c77558ea2fb0c30dc3717bd199f7c89fad1ad84
Mirroy2003 <luis.eduardo.200325@gmail.com>
2024-10-22 05:29:53 -0500

    feat(routes): add /notificaciones route for managing notifications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {ContainerComponent} from "./containers/page/container/container.component";
 import {NgModule} from "@angular/core";
 import {TemplateItemComponent} from "./containers/components/template-item/template-item.component";
+import {NotificationsComponent} from "./settings/page/notifications/notifications.component";
 
 export const routes: Routes = [
   {
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'templates',
     component: TemplateItemComponent,
+  },
+  {
+    path: 'notifications',
+    component: NotificationsComponent,
   },
 
   { path: '',
