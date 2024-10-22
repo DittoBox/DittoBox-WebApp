031529282d0c5194e4d0e2ddab037d8dcc76ce23
Mirroy2003 <luis.eduardo.200325@gmail.com>
2024-10-22 05:35:12 -0500

    feat: add notification service component

---
+++ src/app/settings/service/setting-service.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingServiceService {
+  Baseurl: string = 'http://localhost:3000';
+  constructor(private http: HttpClient) { }
+
+  getNotifications() : Observable<any> {
+    return this.http.get<any>(`${this.Baseurl}/notifications`);
+  }
+
+
+
+}
+
+
