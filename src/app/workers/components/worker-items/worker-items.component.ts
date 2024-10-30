4585f3b35cd456982f287b6c4e49f6e29f044a51
ChamorroSamuel <102174595+ChamorroSamuel@users.noreply.github.com>
2024-10-30 16:53:00 -0500

    update worker component
    update worker component

--- src/app/workers/components/worker-items/worker-items.component.ts
+++ src/app/workers/components/worker-items/worker-items.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {Facility} from "../../../facilities/model/facility-model/facility.model";
+import {FacilityServiceService} from "../../../facilities/service/facility-service.service";
 
 @Component({
   selector: 'app-worker-items',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTabGroup,
+    MatTab,
+  ],
   templateUrl: './worker-items.component.html',
   styleUrl: './worker-items.component.css'
 })
-export class WorkerItemsComponent {
+export class WorkerItemsComponent implements OnInit{
+  allFacilities: Facility[] = [];
+  activeFacility: Facility[] = [];
 
+  constructor(private facilityService: FacilityServiceService) {}
+
+  ngOnInit() {
+    this.loadFacilities();
+  }
+
+  loadFacilities() {
+    this.facilityService.getFacilities().subscribe((data: Facility[]) => {
+      this.allFacilities = data;
+      this.activeFacility = data.filter(facility => facility.status === 'Active');
+    });
+  }
 }
