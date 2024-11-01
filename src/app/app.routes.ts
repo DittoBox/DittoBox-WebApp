1a2bfc5d3c7dd8d994a55ffb13306979475bc250
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2024-10-31 20:15:11 -0500

    fix: Improve component styles and structure; add support for loading status in container service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ import {NgModule} from "@angular/core";
 import {TemplateItemComponent} from "./containers/components/template-item/template-item.component";
 import {FacilityComponent} from "./facilities/page/facility/facility.component";
 import {WorkerComponent} from "./workers/page/worker/worker.component";
+import { NotificationsComponent } from './settings/page/notifications/notifications.component';
+import { AccountComponent } from './settings/page/account/account.component';
+import { SubscriptionComponent } from './settings/page/subscription/subscription.component';
+import { TemplateComponent } from './containers/page/template/template.component';
 
 export const routes: Routes = [
   {
@@ -12,7 +16,7 @@ export const routes: Routes = [
   },
   {
     path: 'templates',
-    component: TemplateItemComponent,
+    component: TemplateComponent,
   },
   {
     path: 'facilities',
@@ -22,6 +26,8 @@ export const routes: Routes = [
   {
     path: 'workers',
     component: WorkerComponent,
+  },
+  {
     path: 'notifications',
     component: NotificationsComponent,
   },
--- src/app/containers/service/container-service.service.ts
+++ src/app/containers/service/container-service.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {BehaviorSubject, forkJoin, Observable} from "rxjs";
-import {Template} from "../model/template-model/template.entity";
-import {Container} from "../model/container-model/container.entity";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, Observable } from "rxjs";
+import { tap, finalize } from 'rxjs/operators';
+import { Template } from "../model/template-model/template.entity";
+import { Container } from "../model/container-model/container.entity";
 
 @Injectable({
   providedIn: 'root'
@@ -15,24 +16,49 @@ export class ContainerServiceService {
   private templateSource = new BehaviorSubject<any>(null);
   templateSelected = this.templateSource.asObservable();
 
+  // to manage loading state
+  private loadingSource = new BehaviorSubject<boolean>(false);
+  loading$ = this.loadingSource.asObservable();
+
   constructor(private http: HttpClient) { }
 
-  getContainers() : Observable<any> {
-    return this.http.get<any>(`${this.Baseurl}/containers`);
+  private setLoading(loading: boolean) {
+    this.loadingSource.next(loading);
   }
-  getTemplates() : Observable<any> {
-    return this.http.get<any>(`${this.Baseurl}/templates`);
-  }
-  getContainerbyId(containerID:string):Observable<any> {
-    return this.http.get<any>(`${this.Baseurl}/containers/${containerID}`);
 
+  getContainers(): Observable<any> {
+    this.setLoading(true);
+    return this.http.get<any>(`${this.Baseurl}/containers`).pipe(
+      finalize(() => this.setLoading(false))
+    );
   }
+
+  getTemplates(): Observable<any> {
+    this.setLoading(true);
+    return this.http.get<any>(`${this.Baseurl}/templates`).pipe(
+      finalize(() => this.setLoading(false))
+    );
+  }
+
+  getContainerbyId(containerID: string): Observable<any> {
+    this.setLoading(true);
+    return this.http.get<any>(`${this.Baseurl}/containers/${containerID}`).pipe(
+      finalize(() => this.setLoading(false))
+    );
+  }
+
   getTemplateById(templateId: string): Observable<any> {
-    return this.http.get<any>(`${this.Baseurl}/templates/${templateId}`);
+    this.setLoading(true);
+    return this.http.get<any>(`${this.Baseurl}/templates/${templateId}`).pipe(
+      finalize(() => this.setLoading(false))
+    );
   }
 
   updateContainer(container: Container) {
-    return this.http.put<Container>(`${this.Baseurl}/containers/${container.id}`, container);
+    this.setLoading(true);
+    return this.http.put<Container>(`${this.Baseurl}/containers/${container.id}`, container).pipe(
+      finalize(() => this.setLoading(false))
+    );
   }
 
   selectTemplate(template: Template) {
@@ -40,7 +66,9 @@ export class ContainerServiceService {
   }
 
   postTemplate(template: Template) {
-    return this.http.post<Template>(`${this.Baseurl}/templates`, template);
+    this.setLoading(true);
+    return this.http.post<Template>(`${this.Baseurl}/templates`, template).pipe(
+      finalize(() => this.setLoading(false))
+    );
   }
-
 }
