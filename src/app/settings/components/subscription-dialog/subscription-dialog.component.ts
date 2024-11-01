8cce8dc2fe702918a0d43e68137b8c6ef4e54162
dcancho <d.cancho.c@gmail.com>
2024-11-01 09:20:17 -0500

    feat: add subscription usage service

--- src/app/settings/components/subscription-dialog/subscription-dialog.component.ts
+++ src/app/settings/components/subscription-dialog/subscription-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { SettingServiceService } from '../../service/setting-service.service';
+import { AccountUsage } from '../../model/subscription/subscription.entity';
 
 @Component({
 	selector: 'app-subscription-dialog',
@@ -14,4 +16,11 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class SubscriptionDialogComponent {
 	readonly dialogRef = inject(MatDialogRef<SubscriptionDialogComponent>);
+	accountUsage: AccountUsage = new AccountUsage();
+
+	constructor(private settingService: SettingServiceService) {
+		this.settingService.getAccountUsage().subscribe((data) => {
+			this.accountUsage = data;
+		});
+	}
 }
