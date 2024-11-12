6555eba5c29e4eb14b724296f294127098c9e97e
ChamorroSamuel <102174595+ChamorroSamuel@users.noreply.github.com>
2024-11-11 23:01:29 -0500

    facility -service

--- src/app/facilities/service/facility-service.service.ts
+++ src/app/facilities/service/facility-service.service.ts
@@ -7,13 +7,13 @@ import { Facility } from '../model/facility-model/facility.model';
   providedIn: 'root'
 })
 export class FacilityServiceService {
-  Baseurl: string = 'https://app-prod-01-dittobox-argeesg8era0c7ex.eastus-01.azurewebsites.net/api/v1/group';
+  Baseurl: string = 'https://app-dev-01-dittobox-a8bpd5bkh4dnh3g7.eastus-01.azurewebsites.net/api/v1/group';
 
   constructor(private http: HttpClient) {}
 
   // Método para obtener un solo facility por ID
-  getFacilityById(facilityID: string): Observable<Facility> {
-    return this.http.get<Facility>(`${this.Baseurl}/${facilityID}`);
+  getFacilityById(groupId: string): Observable<Facility> {
+    return this.http.get<Facility>(`${this.Baseurl}/${groupId}`);
   }
 
   // Método para obtener todos los facilities
