23b6a07766c6deb91bb4b914552c15f05313b4b2
ChamorroSamuel <102174595+ChamorroSamuel@users.noreply.github.com>
2024-11-11 23:01:43 -0500

    facility

--- src/app/facilities/service/facility-service.service.ts
+++ src/app/facilities/service/facility-service.service.ts
@@ -7,7 +7,11 @@ import { Facility } from '../model/facility-model/facility.model';
   providedIn: 'root'
 })
 export class FacilityServiceService {
+<<<<<<< Updated upstream
   Baseurl: string = 'https://app-dev-01-dittobox-a8bpd5bkh4dnh3g7.eastus-01.azurewebsites.net/api/v1/group';
+=======
+  Baseurl: string = 'https://app-dev-01-dittobox-argeesg8era0c7ex.eastus-01.azurewebsites.net/api/v1/group';
+>>>>>>> Stashed changes
 
   constructor(private http: HttpClient) {}
 
