b156af041fcbad6d80f60a72d9221e8b9b038bb9
ChamorroSamuel <102174595+ChamorroSamuel@users.noreply.github.com>
2024-11-14 13:05:16 -0500

    reverted 1

--- src/app/facilities/components/facility-details/facility-details.component.ts
+++ src/app/facilities/components/facility-details/facility-details.component.ts
@@ -31,10 +31,10 @@ import {MatDialog} from "@angular/material/dialog";
   styleUrl: './facility-details.component.css'
 })
 export class FacilityDetailsComponent {
-  facility: any; // Suponiendo que ya tienes cargado el objeto facility con sus datos
+  facility: Facility | null = null;
   opened: boolean = false;
 
-  constructor(private facilityService: FacilityServiceService, public dialog: MatDialog) {}
+  constructor(private facilityService: FacilityServiceService) {}
 
   loadFacility(facilityId: number) {
     const accountId = localStorage.getItem('accountId'); // Obtener el accountId del localStorage
@@ -43,33 +43,6 @@ export class FacilityDetailsComponent {
         this.facility = facilities.find(facility => facility.id === facilityId) || null;
         this.opened = !!this.facility;
       });
-    }
-  }
-
-  openAddContainerDialog() {
-    const dialogRef = this.dialog.open(AddContainerDialogComponent, {
-      width: '300px',
-      data: { accountId: this.facility.accountId }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.addContainer(result);
-      }
-    });
-  }
-
-  addContainer(containerData: any) {
-    if (this.facility && this.facility.id) {
-      this.facilityService.registerContainer(this.facility.id, containerData).subscribe(
-        response => {
-          console.log('Container registered successfully:', response);
-          // Aquí puedes actualizar el conteo de contenedores si es necesario
-        },
-        error => {
-          console.error('Error registering container:', error);
-        }
-      );
     }
   }
 }
