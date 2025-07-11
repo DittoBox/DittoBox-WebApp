54ef14c796fdc6d9536e8a16a86b6d239a359779
dcancho <d.cancho.c@gmail.com>
2025-07-11 02:49:01 -0500

    fix: restore base URL to production endpoint for BaseService

--- src/app/shared/service/base.service.ts
+++ src/app/shared/service/base.service.ts
@@ -4,6 +4,5 @@ import { Injectable } from '@angular/core';
 	providedIn: 'root'
 })
 export class BaseService {
-	// protected baseUrl = 'https://webservices.mydittobox.net';
-	protected baseUrl = 'http://localhost:5007';
+	protected baseUrl = 'https://webservices.mydittobox.net';
 }
