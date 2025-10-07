8c103a01dca9598ece6b09579ea362281477c67e
Diego Cancho <102440160+dcancho@users.noreply.github.com>
2025-10-07 00:14:23 -0500

    Update base URL in BaseService

--- src/app/shared/service/base.service.ts
+++ src/app/shared/service/base.service.ts
@@ -4,5 +4,5 @@ import { Injectable } from '@angular/core';
 	providedIn: 'root'
 })
 export class BaseService {
-	protected baseUrl = 'https://webservices.mydittobox.net';
+	protected baseUrl = 'https://dittobox-webservices.azurewebsites.net';
 }
