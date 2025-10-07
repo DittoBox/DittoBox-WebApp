89df5b3773489f1d1639ff0c517d3d5bdb7c9790
Diego Cancho <102440160+dcancho@users.noreply.github.com>
2025-10-07 00:32:29 -0500

    Update baseUrl to include API version

--- src/app/shared/service/base.service.ts
+++ src/app/shared/service/base.service.ts
@@ -4,5 +4,5 @@ import { Injectable } from '@angular/core';
 	providedIn: 'root'
 })
 export class BaseService {
-	protected baseUrl = 'https://dittobox-webservices.azurewebsites.net';
+	protected baseUrl = 'https://dittobox-webservices.azurewebsites.net/api/v1';
 }
